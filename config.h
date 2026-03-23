60b81f4e4ea4cfe26cabcedd37ade418c7bad827
Misha Bespalov <zelerrlph@gmail.com>
2026-03-23 19:35:16 +0300

    feat: new hrm timings

--- config.h
+++ config.h
@@ -3,13 +3,13 @@
 #define MASTER_LEFT
 
 // Home-row mod timing
-#define TAPPING_TERM 500
+#define TAPPING_TERM 250
 #define QUICK_TAP_TERM 175
 #define PERMISSIVE_HOLD
 #define CHORDAL_HOLD
 
 // Combos
-#define COMBO_TERM 30
+#define COMBO_TERM 40
 #define COMBO_ONLY_FROM_LAYER 0
 
 // Mouse keys — scroll wheel
